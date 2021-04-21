5495e5b2a346cff0404f1f0a2d52db06a0ad6469
Brandon Lok <brandonlok@Brandons-MacBook-Pro.local>
2021-04-20 21:03:34 -0400

    toggle dark styled

--- pages/dashboard/[tab].js
+++ pages/dashboard/[tab].js
@@ -4,15 +4,16 @@ import UserControlSidebar from "../../components/UserControlSidebar";
 import UserCenterMain from "../../components/UserCenterMain";
 import { useRouter } from "next/router";
 import { useAuth } from "../../utils/auth";
-import {useDark} from "../../utils/darkContext";
+import { useDark } from "../../utils/darkContext";
 import useUser from "../../components/Queries/USERS/useUser";
+import MoonSVG from "../../styles/svg/moon.svg";
+import SunSVG from "../../styles/svg/sun.svg";
 
 import { useQueryClient } from "react-query";
 import UserSidebarPlaceholder from "../../components/Placeholders/UserSidebarPlaceholder";
 import SocialSidebarPlaceholder from "../../components/Placeholders/SocialSidebarPlaceholder";
 import CenterMainPlaceholder from "../../components/Placeholders/CenterMainPlaceholder";
 //user dashboard do not require prerendering. but serverside rendering can aide
-
 
 function Dashboard() {
   const { userId } = useAuth();
@@ -34,17 +35,35 @@ function Dashboard() {
     }
   }, [trailing]);
 
+  const [style, setStyle] = useState("bg-gray-300 pl-6 text-yellow-200");
+
+  const toggle = () => {
+    if (dark) {
+      setStyle("bg-gray-300 pl-1 text-yellow-500");
+      setDark(!dark);
+    } else {
+      setStyle("bg-gray-600 pl-6 text-yellow-200");
+      setDark(!dark);
+    }
+  };
+
   if (userData.isSuccess && isReady2) {
     return (
-      <div className={`${darkMode ? "dark" : ""} flex flex-col h-screen w-screen`}>
+      <div className={`flex flex-col h-screen w-screen`}>
         <nav className="h-10 bg-base-gray flex-shrink-0 bg-base-gray dark:bg-cb-3  dark:text-white flex items-center px-4 flex justify-between items-center py-2">
           <div>
             <b>Disqlos</b>
           </div>
-          <div className="rounded-full bg-gray-300 w-10 h-full ">
-          <button onClick={() => setDark(!dark)}>ew</button>
+          <div
+            className={`rounded-full  w-12 h-full flex items-center px-0.5 transition-all ${style}`}
+          >
+            <button
+              onClick={() => toggle()}
+              className="h-5 w-5 rounded-full bg-blue-400 focus:outline-none transition-all flex items-center justify-center p-1 "
+            >
+              {dark ? <MoonSVG className="fill-current" /> : <SunSVG className="fill-current"/>}
+            </button>
           </div>
-          
         </nav>
         <div className="w-full h-full flex flex-row dark:bg-cb-1 flex-initial overflow-hidden">
           <UserControlSidebar userData={userData} />
@@ -56,7 +75,7 @@ function Dashboard() {
     //verify there is content
   } else if (userData.isLoading || !isReady2) {
     return (
-      <div className={`${darkMode ? "dark" : ""} relative flex flex-col h-screen w-screen`}>
+      <div className={` relative flex flex-col h-screen w-screen`}>
         <nav className="h-10 bg-base-gray flex-shrink-0 bg-base-gray flex items-center px-4">
           <div>
             <b>Disqlos</b>
